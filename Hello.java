b9909a7a27773c796f358fed8fb4402fc2d53494
unknown <sandhya rawat>
2020-08-10 17:25:37 -0400

    commit

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello{
 
 public static void main(String[ ] args)
 {
-for(int i=0;i<10;i++)
+for(int i=0;i<10;i++){
 System.out.println("hello world "+i);
 System.out.println("hello git "+i);
-
+}
 }
 }
